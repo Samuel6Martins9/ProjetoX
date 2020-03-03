12367c1751f0f54591c8a4d8009671ac7d8ff958
Samuel6Martins9 <samueldsm100@outlook.com>
2020-03-03 10:44:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+var app = express()
+var bodyparser = require('body-parser')
+var Aluno = require('./model/aluno')
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: false}))
+
+app.set('view engine','ejs')
+
+app.get('/', function(req,res){
+    res.render('listar.ejs',{})
+})
+
+app.get('/add', function(req,res){
+    res.render('adicionar.ejs',{})
+})
+
+app.post('/add', function(req,res){
+    var aluno = new Aluno({
+        nome: req.body.nome,
+        endereco: req.body.endereco,
+        telefone: req.body.telefone
+    })
+    aluno.save(function(err){
+        Aluno.find({}).exec(function(e,docs){;
+        if(err){
+            res.render('listar.ejs', { msg: err, listaAlunos: docs })
+        } else {
+            res.render('listar.ejs', { msg: "Salvo com sucesso", listaAlunos: docs})
+        }
+        })
+    })
+})
+
+app.get('/edit', function(req,res){
+    res.render('editar.ejs',{})
+})
+
+app.listen(3000,function(){
+    console.log("Estou escutando na porta 3000!!")
+})
